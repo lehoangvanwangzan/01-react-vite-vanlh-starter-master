b2c8aca0fdb2318814d1524dfcdfd40f6f484805
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-03-05 18:26:52 +0700

    learn video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,17 @@
 const TodoNew = (props) => {
     console.log("Check props:", props)
     const { addNewTodo } = props;
-    // addNewTodo("Van");
+    const HandleClicked = () => {
+        alert("Clicked");
+    }
+    const HandleOnChange = (name) => {
+        console.log("Change", name);
+    }
+
     return (
         <div className='todo-new'>
-            <input type='text' placeholder="Nhập văn bản" />
-            <button> Add Task</button>
+            <input type='text' placeholder="Nhập văn bản" onChange={(event) => HandleOnChange(event.target.value)} />
+            <button onClick={HandleClicked}> Add Task</button>
         </div>
     );
 }
