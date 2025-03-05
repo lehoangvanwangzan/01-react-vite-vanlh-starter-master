d2688d72d3871ba19d34e6d7d93a5fe8915544a4
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-03-05 08:35:00 +0700

    test arrowfuntion

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+  // // }
+  // function App() {
+  //   const [count, setCount] = useState(0)
 
   return (
     <>
