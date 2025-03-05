ecffe108e093d0e4c4cf1a6325de05161fbdd0bc
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-03-05 13:53:27 +0700

    learn video 32

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+            <div className='todo-item'>
+                <span>Nội dung bảng ghi </span>
+                <button> Delete</button>
+                <button> Edit</button>
+            </div>
+        </div>
+    );
+}
+export default TodoData;    
