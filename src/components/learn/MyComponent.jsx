98592c5c220a1df600f355bb3205579bf065c082
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-03-05 12:15:13 +0700

    learn video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,19 @@
 //fragmaent
 import './style.css';
 const MyComponent = () => {
+    // const hoang = 25;
+    // const hoang = " le hoang van";
+    // const hoang = true; 
+    // const hoang = undefined;    
+    // const hoang = null;
+    const hoang = [1, 2, 3, 4, 5];
+    // const hoang = { name: "le hoang van", age: 25 };
+
     return (
         <>
-            <div> hoang van ^ tech shop 1234</div>
+            <div> {hoang.age} van ^ tech shop 1234</div>
+            <div>{JSON.stringify(hoang)} van tech shop 9999</div>
+            <div>{console.log("tech shop 1111")}</div>
             <div className="child" style={{ borderRadius: "10xp" }}> Lê Hoàng Văn update</div>
         </>
     );
