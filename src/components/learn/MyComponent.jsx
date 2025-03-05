1edc0a57c4da93133baa624f4fdaa4310a9fd3a6
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-03-05 11:46:05 +0700

    learn video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+
+//fragmaent
+import './style.css';
 const MyComponent = () => {
     return (
-        <div> hoang van ^ tech shop 1234</div>
+        <>
+            <div> hoang van ^ tech shop 1234</div>
+            <div className="child" style={{ borderRadius: "10xp" }}> Lê Hoàng Văn update</div>
+        </>
     );
 }
 
