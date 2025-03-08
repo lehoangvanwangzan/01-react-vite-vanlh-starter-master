a7feb2daa9bdd8d08341c6620d17859148849e0f
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-03-08 23:33:48 +0700

    learn video 75

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Table } from 'antd';
-import { fetchAllUserAPI } from '../../services/api.service';
-import { useState, useEffect } from "react";
-const UserTable = () => {
-    const [dataUsers, setDataUsers] = useState('');
+
+
+const UserTable = (props) => {
+    const { dataUsers } = props;
     const columns = [
         {
             title: 'ID',
@@ -18,16 +18,7 @@ const UserTable = () => {
             dataIndex: 'email',
         }
     ];
-    //emty array run one
-    useEffect(() => {
-        loadUser();
-        // console.log(">>> run render1111")
-    }, []);
-    const loadUser = async () => {
-        const res = await fetchAllUserAPI();
-        setDataUsers(res.data);
-        // console.log(">>> run render2222")
-    }
+
     // console.log(">>> run render0000")
     return (
         <Table columns={columns} dataSource={dataUsers} rowKey={"_id"} />
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,10 +1,23 @@
 import UserForm from "../components/user/user.form";
 import UserTable from "../components/user/user.table";
+import { useState, useEffect } from "react";
+import { fetchAllUserAPI } from '../services/api.service';
 const UserPage = () => {
+    const [dataUsers, setDataUsers] = useState('');
+    //emty array run one
+    useEffect(() => {
+        loadUser();
+        // console.log(">>> run render1111")
+    }, []); //Biến 2 để mảng rổng thì hàm chỉ chạy 1 lần
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI();
+        setDataUsers(res.data);
+        // console.log(">>> run render2222")
+    }
     return (
         <div style={{ padding: "20px" }}>
-            <UserForm />
-            <UserTable />
+            <UserForm loadUser={loadUser} />
+            <UserTable dataUsers={dataUsers} />
         </div >
     );
 };
